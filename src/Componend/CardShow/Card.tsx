46ba76eee8c4e3c13158f7f33e7d2ede79792fa8
chamok barmon <chamokbarmon123@gmail.com>
2023-10-20 00:00:11 +0600

    card stale

--- src/Componend/CardShow/Card.tsx
+++ src/Componend/CardShow/Card.tsx
@@ -1,38 +1,39 @@
 import { useEffect, useState } from "react";
 
-
 const Card = () => {
-    const [data, setData] = useState([])
-    console.log(data);
-    
+  const [data, setData] = useState([]);
+  console.log(data);
 
-    
-    useEffect(()=>{
-        fetch("https://api.spacexdata.com/v3/launches")
-        .then(res=>res.json())
-        .then(result=>setData(result))
-        .catch(err=>console.log(err))
-    },[])
-    
- 
+  useEffect(() => {
+    fetch("https://api.spacexdata.com/v3/launches")
+      .then((res) => res.json())
+      .then((result) => setData(result))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
-    <div>
-      <div className="card w-96 bg-base-100 shadow-xl">
-        <figure className="px-10 pt-10">
-          <img
-            src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-            alt="Shoes"
-            className="rounded-xl"
-          />
-        </figure>
-        <div className="card-body items-center text-center">
-          <h2 className="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions">
-            <button className="btn btn-primary">Buy Now</button>
+    <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-5 ">
+      {data.splice(0, 9)?.map((show: any) => (
+        <div className="card border w-[424px] h-[393.4px] mt-10 bg-base-100 shadow-xl p-4">
+          <figure className="w-[124px] h-[124px] mx-auto ">
+            <img
+              src={show?.links?.mission_patch_small}
+              alt="rocket"
+              className="rounded-xl"
+            />
+          </figure>
+          <div className="card-body items-center text-center">
+            <h2 className="card-title">
+              Launch Date:{show?.launch_date_local}
+            </h2>
+            <h1>{show?.mission_name}</h1>
+            <h3>{show?.launch_date_utc}</h3>
+            <div className="card-actions">
+              <button className="btn btn-primary">success</button>
+            </div>
           </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
