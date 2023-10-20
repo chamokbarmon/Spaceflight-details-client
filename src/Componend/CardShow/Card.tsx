e4443b59b0e30cbf2ffe06b0fc76b34fab31ecfb
chamok barmon <chamokbarmon123@gmail.com>
2023-10-20 12:25:59 +0600

    pagination setup

--- src/Componend/CardShow/Card.tsx
+++ src/Componend/CardShow/Card.tsx
@@ -2,27 +2,34 @@ import { useEffect, useState } from "react";
 
 const Card = () => {
   const [data, setData] = useState([]);
-  const [page, setPage] = useState(1)
-  const [pageSize, setPageSize] = useState(9)
-
-
+  const [loading, setLoading] = useState(false);
+  const [currentpage, setCurrentPage] = useState(1)
+  const [postperpage, setPostPerPage] = useState(9)
   const dataLength = data.length
 
-  const pages = Math.ceil(dataLength / pageSize);
-  console.log(pages);
+
   
 
   useEffect(() => {
+    setLoading(true)
     fetch("https://api.spacexdata.com/v3/launches")
       .then((res) => res.json())
       .then((result) => setData(result))
       .catch((err) => console.log(err));
   }, []);
 
+ 
+  const lastPost = currentpage * postperpage ;
+  const firstPost = lastPost - postperpage ;
+  const currentPosts =  data.splice(firstPost, lastPost)
+  console.log(currentPosts);
+  
+
+
   return (
    <div>
      <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-5 ">
-      {data?.splice(0,9)?.map((show: any) => (
+      {currentPosts?.map((show: any) => (
         <div className="card border w-[424px] h-[393.4px] mt-10 bg-base-100 shadow-xl p-4">
           <figure className="w-[124px] h-[124px] mx-auto ">
             <img
@@ -38,24 +45,14 @@ const Card = () => {
             <h1>{show?.mission_name}</h1>
             <h3>{show?.launch_date_utc}</h3>
             <div className="card-actions">
-              <button className="btn btn-primary">success</button>
+              <button className="btn bg-[#198754] text-white">success</button>
             </div>
           </div>
         </div>
       ))}
       
     </div>
-    <div className="m-auto flex justify-center mt-20 align-middle ">
-      
-        {
-            [...Array(pages).keys()].map(number=>
-            <button className="btn btn-active" key={number}
-            onClick={()=>setPage(number)}
-            >
-            {number}
-            </button>)
-        }
-      </div>
+   
    </div>
   );
 };
