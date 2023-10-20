5caadeb013c4904c13dd9bef759b59f582b67655
chamok barmon <chamokbarmon123@gmail.com>
2023-10-20 09:26:38 +0600

    pagination setup

--- src/Componend/CardShow/Card.tsx
+++ src/Componend/CardShow/Card.tsx
@@ -2,7 +2,15 @@ import { useEffect, useState } from "react";
 
 const Card = () => {
   const [data, setData] = useState([]);
-  console.log(data);
+  const [page, setPage] = useState(1)
+  const [pageSize, setPageSize] = useState(9)
+
+
+  const dataLength = data.length
+
+  const pages = Math.ceil(dataLength / pageSize);
+  console.log(pages);
+  
 
   useEffect(() => {
     fetch("https://api.spacexdata.com/v3/launches")
@@ -12,8 +20,9 @@ const Card = () => {
   }, []);
 
   return (
-    <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-5 ">
-      {data.splice(0, 9)?.map((show: any) => (
+   <div>
+     <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-5 ">
+      {data?.splice(0,9)?.map((show: any) => (
         <div className="card border w-[424px] h-[393.4px] mt-10 bg-base-100 shadow-xl p-4">
           <figure className="w-[124px] h-[124px] mx-auto ">
             <img
@@ -34,7 +43,20 @@ const Card = () => {
           </div>
         </div>
       ))}
+      
     </div>
+    <div className="m-auto flex justify-center mt-20 align-middle ">
+      
+        {
+            [...Array(pages).keys()].map(number=>
+            <button className="btn btn-active" key={number}
+            onClick={()=>setPage(number)}
+            >
+            {number}
+            </button>)
+        }
+      </div>
+   </div>
   );
 };
 
